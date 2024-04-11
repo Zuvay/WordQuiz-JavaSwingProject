0c9298f2ae425bbe78f7e86310d4454f052f38b4
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-04-11 19:07:07 +0300

    Update MistakesWordQuiz.java

--- WordQuiz/src/MistakesWordQuiz.java
+++ WordQuiz/src/MistakesWordQuiz.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;.
+import java.util.List;
 
 public class MistakesWordQuiz extends JFrame{
     private JPanel QuizPanel;
