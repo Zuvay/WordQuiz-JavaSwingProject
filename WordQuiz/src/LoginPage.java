c1446c89cd332d3e0d584e215016f4e16020298c
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-04-22 01:29:47 +0300

    register ve login formları eklendi

---
+++ WordQuiz/src/LoginPage.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class LoginPage {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton registerButton;
+    private JButton logInButton;
+    private JLabel warningLabel;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+}
---
+++ WordQuiz/src/RegisterPage.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class RegisterPage {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JTextField textField2;
+    private JPasswordField passwordField2;
+    private JButton registerButton;
+    private JLabel passwordMatchLabel;
+}
