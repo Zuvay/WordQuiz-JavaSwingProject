1d2c1b7cbb6562254ee49ab0f3ec04901fc710c7
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-04-28 16:08:44 +0300

    Registir olabilmek için şartlar eklendi. (Email,username daha önce kullanılmamış olmalı,Email formatı doğru olmalı ve şifre 6 karakterden uzun olmalı).

--- WordQuiz/src/RegisterPage.java
+++ WordQuiz/src/RegisterPage.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterPage extends JFrame{
     private JTextField usernameField;
     private JPasswordField passwordField1;
@@ -54,15 +62,94 @@ public class RegisterPage extends JFrame{
     public static boolean isRegisterInformationFalse(String username,String email,String password,String confirmPassword,JLabel warningLabel){
         if (username.isEmpty()) {
             warningLabel.setText("Username required");
-        } else if (password.isEmpty()) {
+        }else if(usernameExist(username)){
+            warningLabel.setText("This username is already taken");
+        }else if (password.isEmpty()){
             warningLabel.setText("Password required");
-        } else if (email.isEmpty()) {
+        }else if (isPasswordNotLongEnough(password)){
+            warningLabel.setText("Password needs to be at least 6 characters  long");
+        }else if (email.isEmpty()){
             warningLabel.setText("Email required");
-        } else if (!password.equals(confirmPassword)) {
+        }else if (isMailAdressFormatFalse(email)){
+            warningLabel.setText("Please enter a valid email address");
+        }else if (mailAdressExist(email)){
+            warningLabel.setText("This mail adress is already taken");
+        }else if (!password.equals(confirmPassword)) {
             warningLabel.setText("Passwords doesn't match");
-        }else{
+        }else {
             return false;
         }
         return true;
+    }
+    public static boolean usernameExist(String username){
+        DbHelper dbHelper = new DbHelper();
+        try (Connection connection = dbHelper.getConnection()) {
+            String sql = "SELECT username FROM dictionary.users";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            //isimleri array list ile tutmak
+            ArrayList<String> usernames = new ArrayList<>();
+            while (resultSet.next()) {
+                //username  tanımlamak
+                String usernameFromDb = resultSet.getString("username");
+                //Döngüdeki her bir veriyi HashMap'e ekle
+                usernames.add(usernameFromDb);
+            }
+
+            for (String user : usernames){
+                if (user.equals(username)){
+                    return true;
+                }
+            }
+        } catch (SQLException exception) {
+            dbHelper.showErrorException(exception);
+        }
+
+        return false;
+    }
+    public static boolean mailAdressExist(String email){
+        DbHelper dbHelper = new DbHelper();
+        try (Connection connection = dbHelper.getConnection()) {
+            String sql = "SELECT email FROM dictionary.users";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            //isimleri array list ile tutmak
+            ArrayList<String> emails = new ArrayList<>();
+            while (resultSet.next()) {
+                //username  tanımlamak
+                String emailFromDb = resultSet.getString("email");
+                //Döngüdeki her bir veriyi HashMap'e ekle
+                emails.add(emailFromDb);
+            }
+
+            for (String mailAdress : emails){
+                if (mailAdress.equals(email)){
+                    return true;
+                }
+            }
+        } catch (SQLException exception) {
+            dbHelper.showErrorException(exception);
+        }
+
+        return false;
+    }
+    public static boolean isMailAdressFormatFalse(String email){
+        String registirationInfo = email;
+        //Tüm email formatlarını yakalayabilmesi için bu yazımı internetten aldım
+        Pattern pattern = Pattern.compile("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b");
+        Matcher matcher = pattern.matcher(registirationInfo);
+
+        if (matcher.matches()){
+            return false;
+        }
+        return true;
+    }
+    public static boolean isPasswordNotLongEnough(String password){
+        if (password.length()<6){
+            return true;
+        }
+        return false;
     }
 }
